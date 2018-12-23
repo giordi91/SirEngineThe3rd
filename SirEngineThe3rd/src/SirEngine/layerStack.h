04ead04db0772c06c53c355c6ed06fa7e5e671f9
Marco Giordano <marco.giordano.work@gmail.com>
2018-12-23 00:00:00 +0000

    added layer stack

---
+++ SirEngineThe3rd/src/SirEngine/layerStack.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#pragma once
+#include "SirEngine/core.h"
+#include "SirEngine/events/event.h"
+#include <string>
+
+namespace SirEngine {
+
+class Layer;
+class SIR_ENGINE_API LayerStack {
+public:
+  LayerStack();
+  ~LayerStack();
+
+  void pushLayer(Layer* layer);
+  void popLayer(Layer* layer);
+
+  void pushOverlayLayer(Layer* layer);
+  void popOverlayLayer(Layer* layer);
+
+  std::vector<Layer*>::iterator begin() { return m_layers.begin(); };
+  std::vector<Layer*>::iterator end() { return m_layers.end(); };
+private:
+  std::vector<Layer*> m_layers;
+  std::vector<Layer*>::iterator m_layerInsert;
+};
+} // namespace SirEngine
