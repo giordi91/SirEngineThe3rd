b7fbb9fe124229e473433d0354bce7ad1286ad28
Marco Giordano <marco.giordano.work@gmail.com>
2019-01-13 18:28:30 +0000

    added resource compiler

---
+++ ResourceCompilerLib/resourceCompilerLib/resourcePlugin.h
@@ -0,0 +1,36 @@
+#include "core.h"
+#include <string>
+#include <unordered_map>
+#include <Windows.h>
+
+typedef bool (*ResourceProcessFunction)(const std::string &assetPath,
+                                        const std::string &outputPath,
+										const std::string &pluginArgs);
+class PluginRegistry {
+public:
+  static RC_API void init();
+  RC_API void clear();
+  static RC_API PluginRegistry *getInstance() { return registryInst; }
+
+  void RC_API registerFunction(const std::string &name,
+                               ResourceProcessFunction func);
+  RC_API void loadPlugin(const std::string &dllPath);
+  RC_API ResourceProcessFunction 
+  getFunction(const std::string &name) const {
+    auto found = m_registry.find(name);
+    if (found != m_registry.end()) {
+      return found->second;
+    }
+    return nullptr;
+  }
+  RC_API void loadPluginsInFolder(const std::string& path);
+
+  PluginRegistry();
+
+private:
+  std::unordered_map<std::string, ResourceProcessFunction> m_registry;
+  std::vector<HMODULE> m_dlls;
+  static PluginRegistry *registryInst;
+};
+
+typedef bool (*RegisterPluginFunction)(PluginRegistry *registry);
