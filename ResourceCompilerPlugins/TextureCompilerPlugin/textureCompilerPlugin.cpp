04dfb9b2aa61ba59eb727bcdda340c62db80df05
Marco Giordano <marco.giordano.work@gmail.com>
2019-02-01 12:22:54 +0000

    fixed texture compiler arguments, now can compile correctly gamma and not

--- ResourceCompilerPlugins/TextureCompilerPlugin/textureCompilerPlugin.cpp
+++ ResourceCompilerPlugins/TextureCompilerPlugin/textureCompilerPlugin.cpp
@@ -11,19 +11,25 @@ const unsigned int VERSION_MAJOR = 0;
 const unsigned int VERSION_MINOR = 1;
 const unsigned int VERSION_PATCH = 0;
 
-void processArgs(const std::string args, std::string &format) {
+void processArgs(const std::string args, std::string &format, bool &isGamma) {
   // lets get arguments like they were from commandline
   auto v = splitArgs(args);
   // lets build the options
   cxxopts::Options options("Texture compiler",
                            "Converts a texture in DDS format");
   options.add_options()("f,format", "output format for the texture",
-                        cxxopts::value<std::string>());
+                        cxxopts::value<std::string>())(
+      "g,gamma", "whether the texture is to considered gamma corrected or not",
+      cxxopts::value<std::string>()->implicit_value("1"));
   char **argv = v.argv.get();
   auto result = options.parse(v.argc, argv);
 
   if (result.count("format")) {
     format = result["format"].as<std::string>();
+  }
+  isGamma = false;
+  if (result.count("gamma")) {
+    isGamma = true;
   }
 }
 
@@ -31,8 +37,9 @@ bool processTexture(const std::string &assetPath, const std::string &outputPath,
                     const std::string &args) {
 
   // processing plugins args
-  std::string format = "";
-  processArgs(args, format);
+  std::string format{""};
+  bool isGamma;
+  processArgs(args, format, isGamma);
 
   // checking IO files exits
   bool exits = fileExists(assetPath);
@@ -46,7 +53,7 @@ bool processTexture(const std::string &assetPath, const std::string &outputPath,
     SE_CORE_ERROR("[Texture Compiler] : could not find path/file {0}",
                   outputPath);
   }
-  bool res = loadTextureFromFile(assetPath.c_str(), outputPath.c_str(), format);
+  bool res = processTextureFile(assetPath.c_str(), outputPath.c_str(), format,isGamma);
 
   if (res) {
     SE_CORE_INFO("Texture successfully compiled ---> {0}", outputPath);
