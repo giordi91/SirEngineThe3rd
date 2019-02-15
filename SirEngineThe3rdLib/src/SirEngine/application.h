e463ffd70c5fbe49746874bf79f1d13f93a84c41
Marco Giordano <marco.giordano.work@gmail.com>
2019-02-15 17:24:13 +0000

    queuing events to end of frame

--- SirEngineThe3rdLib/src/SirEngine/application.h
+++ SirEngineThe3rdLib/src/SirEngine/application.h
@@ -5,6 +5,7 @@
 #include "SirEngine/events/applicationEvent.h"
 #include "SirEngine/events/event.h"
 #include "SirEngine/layerStack.h"
+#include <vector>
 namespace SirEngine {
 class SIR_ENGINE_API Application {
 public:
@@ -13,19 +14,21 @@ public:
   void run();
 
   void onEvent(Event &e);
+  void queueEventForEndOfFrame(Event *e);
   void pushLayer(Layer *layer);
   void pushOverlay(Layer *layer);
 
 private:
   bool onCloseWindow(WindowCloseEvent &e);
-  bool onResizeWindow(WindowResizeEvent&e);
+  bool onResizeWindow(WindowResizeEvent &e);
 
 private:
+  std::vector<Event*> m_queuedEndOfFrameEvents;
   Window *m_window = nullptr;
   bool m_run = true;
   LayerStack m_layerStack;
-  Layer* imGuiLayer;
-  Layer* graphicsLayer;
+  Layer *imGuiLayer;
+  Layer *graphicsLayer;
 };
 
 // To be implemented by the client
