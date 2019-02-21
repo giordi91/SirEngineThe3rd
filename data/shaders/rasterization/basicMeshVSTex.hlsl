05c17cf139c03b85a7e8c4a1eb8a10b141654f33
Marco Giordano <marco.giordano.work@gmail.com>
2019-02-21 00:00:00 +0000

    about to change memory layout of meshes

--- data/shaders/rasterization/basicMeshVSTex.hlsl
+++ data/shaders/rasterization/basicMeshVSTex.hlsl
@@ -1,24 +1,12 @@
-struct CameraBuffer {
-  float4x4 MVP;
-  float4x4 ViewMatrix;
-  float vFov;
-  float screenWidth;
-  float screenHeight;
-  float padding;
-};
+#include "../common/camera.hlsl"
 
 ConstantBuffer<CameraBuffer> g_cameraBuffer : register(b0);
-//cbuffer cbPerObject : register(b0)
-//{
-//	float4x4 gWorldViewProj; 
-//};
-
 
 struct VertexIn
 {
-	float4 PosL  : POSITION;
-    float4 Normal : NORMAL;
-    float4 uvs: TEXCOORD;
+	float3 PosL  : POSITION;
+    float3 Normal : NORMAL;
+    float2 uvs: TEXCOORD;
     float4 tangents: TANGENTS;
 };
 
