6b4e00b363ca8dc128199798c95f4d8ff32b1a1e
Marco Giordano <marco.giordano.work@gmail.com>
2019-03-02 19:22:23 +0000

    modified shader sky for better gradient

--- data/shaders/rasterization/proceduralSkyboxPS.hlsl
+++ data/shaders/rasterization/proceduralSkyboxPS.hlsl
@@ -12,16 +12,24 @@ float4 PS(FullScreenVertexOut pin) : SV_Target
     float3 ground = float3(0.412f, 0.380, 0.357);
     float3 skyTop = float3(0.357f, 0.451f, 0.6f);
     float3 skybottom = float3(0.906f, 1.0f, 1.0f);
+
+    float bottomGradientDiffusion = 15.0f;
+    float topGradientDiffiusion= 2.0f;
+    float verticalGradient = worldDir.y;
+    float topLerpFactor = saturate(verticalGradient*topGradientDiffiusion);
+    float bottomLerpFactor = saturate(-verticalGradient*bottomGradientDiffusion);
+
+    //sky
     if(worldDir.y > 0)
     {
-        float fractional = frac(worldDir.y);
-        float4 color = float4(lerp(skybottom, skyTop, fractional), 1.0f);
-    return color;
+        float4 color = float4(lerp(skybottom, skyTop, topLerpFactor), 1.0f);
+        return color;
     }
+    //ground
     else
     {
-        float4 color = float4(ground, 1.0f);
-    return color;
+        float4 color = float4(lerp(skybottom, ground, bottomLerpFactor), 1.0f);
+        return color;
     }
 }
 
