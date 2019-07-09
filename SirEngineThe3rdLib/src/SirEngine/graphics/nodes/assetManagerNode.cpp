6f30da4f585436157b85659aa36da5952761cf83
Marco Giordano <marco.giordano.work@gmail.com>
2019-07-09 00:00:00 +0000

    fixed rendergraph with new asset management system, need to start working on translucency

--- SirEngineThe3rdLib/src/SirEngine/graphics/nodes/assetManagerNode.cpp
+++ SirEngineThe3rdLib/src/SirEngine/graphics/nodes/assetManagerNode.cpp
@@ -5,39 +5,17 @@ namespace SirEngine {
 AssetManagerNode::AssetManagerNode()
     : GraphNode("AssetManagerNode", "AssetManagerNode") {
   // lets create the plugs
-  Plug matrices;
-  matrices.plugValue = 0;
-  matrices.flags = PlugFlags::PLUG_OUTPUT | PlugFlags::PLUG_CPU_BUFFER;
-  matrices.nodePtr = this;
-  matrices.name = "matrices";
-  registerPlug(matrices);
-
-  Plug meshes;
-  meshes.plugValue = 0;
-  meshes.flags = PlugFlags::PLUG_OUTPUT | PlugFlags::PLUG_MESHES;
-  meshes.nodePtr = this;
-  meshes.name = "meshes";
-  registerPlug(meshes);
-
-  Plug materials;
-  materials.plugValue = 0;
-  materials.flags = PlugFlags::PLUG_OUTPUT | PlugFlags::PLUG_CPU_BUFFER;
-  materials.nodePtr = this;
-  materials.name = "materials";
-  registerPlug(materials);
+  Plug stream;
+  stream.plugValue = 0;
+  stream.flags = PlugFlags::PLUG_OUTPUT | PlugFlags::PLUG_CPU_BUFFER;
+  stream.nodePtr = this;
+  stream.name = "assetStream";
+  registerPlug(stream);
 }
 
 void AssetManagerNode::compute() {
   // here we need to get handles to the data
   m_outputPlugs[0].plugValue =
-      globals::ASSET_MANAGER->getStaticDataHandle(AssetDataType::MATRICES)
-          .handle;
-
-  m_outputPlugs[1].plugValue =
-      globals::ASSET_MANAGER->getStaticDataHandle(AssetDataType::MESHES).handle;
-
-  m_outputPlugs[2].plugValue =
-      globals::ASSET_MANAGER->getStaticDataHandle(AssetDataType::MATERIALS)
-          .handle;
+      globals::ASSET_MANAGER->getMainStreamHandle().handle;
 }
 } // namespace SirEngine
