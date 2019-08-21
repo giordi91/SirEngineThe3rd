776387b35d0363e5ec3f1c47e1686aa25aaf7d42
Marco Giordano <marco.giordano.work@gmail.com>
2019-08-21 13:50:27 +0100

    changed backend of constant buffer manager to use a pool, should make life easier for freeing stuff

--- SirEngineThe3rdLib/src/platform/windows/graphics/dx12/ConstantBufferManagerDx12.h
+++ SirEngineThe3rdLib/src/platform/windows/graphics/dx12/ConstantBufferManagerDx12.h
@@ -3,6 +3,7 @@
 #include "SirEngine/globals.h"
 #include "SirEngine/handle.h"
 #include "SirEngine/log.h"
+#include "SirEngine/memory/SparseMemoryPool.h"
 #include "SirEngine/memory/randomSizeAllocator.h"
 #include "platform/windows/graphics/dx12/DX12.h"
 #include <vector>
@@ -11,7 +12,7 @@ namespace SirEngine::dx12 {
 
 class ConstantBufferManagerDx12 final : public ConstantBufferManager {
 public:
-  ConstantBufferManagerDx12() = default;
+  ConstantBufferManagerDx12() : m_dynamicStorage(RESERVE_SIZE){} 
   virtual ~ConstantBufferManagerDx12() = default;
   void initialize();
   // deleted method to avoid copy, you can still move it though
@@ -30,14 +31,14 @@ public:
     // making sure the resource has not been de-allocated
     assertMagicNumber(handle);
     const uint32_t index = getIndexFromHandle(handle);
-    return m_dynamicStorage[globals::CURRENT_FRAME][index].pair;
+    return m_dynamicStorage[index].cbData[globals::CURRENT_FRAME].pair;
   }
 
   inline D3D12_GPU_VIRTUAL_ADDRESS
   getVirtualAddress(const ConstantBufferHandle handle) {
     assertMagicNumber(handle);
     uint32_t index = getIndexFromHandle(handle);
-    return m_dynamicStorage[globals::CURRENT_FRAME][index]
+    return m_dynamicStorage[index].cbData[globals::CURRENT_FRAME]
         .resource->GetGPUVirtualAddress();
   }
 
@@ -64,7 +65,7 @@ private:
     // the actual data size, we can't have buffers that big anyway
     uchar *mappedData = nullptr;
     ID3D12Resource *resource = nullptr;
-	uint64_t fence =0;
+    uint64_t fence = 0;
     bool mapped : 1;
     uint32_t size : 31;
     uint32_t magicNumber : 16;
@@ -72,11 +73,15 @@ private:
     DescriptorPair pair;
   };
 
+  struct ConstantBufferDataDynamic {
+    ConstantBufferData cbData[FRAME_BUFFERS_COUNT];
+  };
+
 private:
   inline void assertMagicNumber(const ConstantBufferHandle handle) {
     uint32_t magic = getMagicFromHandle(handle);
     uint32_t idx = getIndexFromHandle(handle);
-    assert(m_dynamicStorage[globals::CURRENT_FRAME][idx].magicNumber == magic &&
+    assert(m_dynamicStorage[idx].cbData[globals::CURRENT_FRAME].magicNumber == magic &&
            "invalid magic handle for constant buffer");
   }
   inline void mapConstantBuffer(ConstantBufferData &data) const {
@@ -90,10 +95,12 @@ private:
   };
 
 private:
-  std::vector<ConstantBufferData> m_dynamicStorage[FRAME_BUFFERS_COUNT];
+  // std::vector<ConstantBufferData> m_dynamicStorage[FRAME_BUFFERS_COUNT];
+
+  SparseMemoryPool<ConstantBufferDataDynamic> m_dynamicStorage;
   static const uint32_t INDEX_MASK = (1 << 16) - 1;
   static const uint32_t MAGIC_NUMBER_MASK = ~INDEX_MASK;
-  static const uint32_t RESERVE_SIZE = 200;
+  static const uint32_t RESERVE_SIZE = 400;
   uint32_t MAGIC_NUMBER_COUNTER = 1;
   RandomSizeAllocator m_randomAlloc;
   std::unordered_map<uint32_t, ConstantBufferedData> m_bufferedRequests;
