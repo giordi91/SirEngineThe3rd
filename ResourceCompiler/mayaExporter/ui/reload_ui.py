5915ac55a27c6e48c6ef978cda45c523e8501d7d
Marco Giordano <marco.giordano.work@gmail.com>
2019-10-16 16:37:06 +0100

    fixed reload

--- ResourceCompiler/mayaExporter/ui/reload_ui.py
+++ ResourceCompiler/mayaExporter/ui/reload_ui.py
@@ -1,27 +1,28 @@
 from ui.helperWidgets import reload_helper_widgets
 from ui.workFile import reload_work_file
 from ui import actionUi
-from ui import sessionUi
 from ui import fileActionUi
 from ui import exportModelActionUi 
 from ui import exportSkinActionUi
 from ui import exportSkeletonActionUi
 from ui import exportAnimActionUi
+from ui import sessionUi
 
 
 
-modules = [reload_helper_widgets, reload_work_file, 
-            sessionUi,exportModelActionUi, exportSkinActionUi,
-            exportSkeletonActionUi, exportAnimActionUi]
+modules = [ actionUi, fileActionUi, exportModelActionUi, exportSkinActionUi,
+            exportSkeletonActionUi, exportAnimActionUi, sessionUi]
 
 reload_it_modules = [reload_helper_widgets, reload_work_file]
 def reload_it():
     print "------> Sarted reload of ui"
+    for sub_module in reload_it_modules:
+        reload(sub_module)
+        sub_module.reload_it()
+
     for sub_module in modules:
         reload(sub_module)
         print "---> Reloading : " +str(sub_module.__name__)
 
-    for sub_module in reload_it_modules:
-        sub_module.reload_it()
 
     print "------> Ended reload of ui"
