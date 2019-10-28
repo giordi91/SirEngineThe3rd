4cedb3587529cf664f154cc81e8201f1f83267dd
Marco Giordano <marco.giordano.work@gmail.com>
2019-10-28 18:06:42 +0000

    fixed search of tags, need to keep working anim interp

--- Tests/src/animationTest.cpp
+++ Tests/src/animationTest.cpp
@@ -20,6 +20,7 @@ void compileAnim(const char *in, const char *out) {
   func(in, out, "");
 }
 
+/*
 TEST_CASE("animation key 1", "[animation]") {
 
   // initialize memory pools and loggers
@@ -136,7 +137,9 @@ TEST_CASE("animation key 2 read", "[animation]") {
 
   SirEngine::Log::free();
 }
+*/
 
+/*
 TEST_CASE("animation key 2 read from frame", "[animation]") {
   // initialize memory pools and loggers
   SirEngine::Log::init();
@@ -185,6 +188,70 @@ TEST_CASE("animation key 2 read from frame", "[animation]") {
   resultFrame = clip->findMetadataFrameFromGivenFrame(
       SirEngine::ANIM_CLIP_KEYWORDS::L_FOOT_DOWN, 35);
   REQUIRE(resultFrame == 30);
+
+  SirEngine::Log::free();
+}
+*/
+
+TEST_CASE("animation key 3 read from frame", "[animation]") {
+  // initialize memory pools and loggers
+  SirEngine::Log::init();
+
+  PluginRegistry::init();
+  PluginRegistry *registry = PluginRegistry::getInstance();
+  registry->loadPluginsInFolder("plugins");
+  // compiling animation clip on the fly, then we read it back as the engine
+  // would
+  compileAnim("../testData/knightBWalk.json", "../testData/knightBWalk.clip");
+
+  SirEngine::StringPool stringPool(1024 * 1024 * 20);
+  SirEngine::ThreeSizesPool pool(1024 * 1024 * 20);
+  SirEngine::globals::STRING_POOL = &stringPool;
+  SirEngine::globals::PERSISTENT_ALLOCATOR = &pool;
+  SirEngine::AnimationManager animManager;
+  animManager.init();
+  SirEngine::globals::ANIMATION_MANAGER = &animManager;
+
+  // now we can read it
+  SirEngine::AnimationClip *clip =
+      animManager.loadAnimationClip("idle2", "../testData/knightBWalk.clip");
+
+  REQUIRE(clip->m_metadataCount == 42);
+  int resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::R_FOOT_DOWN, 30);
+  REQUIRE(resultFrame == 8);
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::R_FOOT_DOWN, 35);
+  REQUIRE(resultFrame == 8);
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::R_FOOT_DOWN, 6);
+  REQUIRE(resultFrame == 8);
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::R_FOOT_DOWN, 10);
+  REQUIRE(resultFrame == 10);
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::L_FOOT_DOWN, 25);
+  REQUIRE(resultFrame == 25);
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::L_FOOT_DOWN, 23);
+  REQUIRE(resultFrame == 25);
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::L_FOOT_DOWN, 30);
+  REQUIRE(resultFrame == 30);
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::L_FOOT_DOWN, 35);
+  REQUIRE(resultFrame == 35);
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::L_FOOT_DOWN, 0);
+  REQUIRE(resultFrame == 0);
+  // although the original animation is from 1 to 36, we don't keep track of the
+  // offset we don't need it, so the effective range is 1-35
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::L_FOOT_DOWN, 10);
+  REQUIRE(resultFrame == 25);
+  resultFrame = clip->findMetadataFrameFromGivenFrame(
+      SirEngine::ANIM_CLIP_KEYWORDS::L_FOOT_DOWN, 9);
+  REQUIRE(resultFrame == 9);
 
   SirEngine::Log::free();
 }
