12155c765002e757373c13bd02dd2e5cff13b990
Marco Giordano <marco.giordano.work@gmail.com>
2019-11-12 00:00:00 +0000

    added pcs with multitaps for shadows, fixed pattern might want to randomize it

--- ResourceCompilerPlugins/RootSignatureCompilerPlugin/rootProcess.cpp
+++ ResourceCompilerPlugins/RootSignatureCompilerPlugin/rootProcess.cpp
@@ -83,7 +83,7 @@ D3D12_SHADER_VISIBILITY getVisibility(const nlohmann::json &jobj) {
   return toReturn;
 }
 
-std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> getStaticSamplers() {
+std::array<const CD3DX12_STATIC_SAMPLER_DESC, 7> getStaticSamplers() {
   // Applications usually only need a handful of samplers.  So just define them
   // all up front and keep them available as part of the root signature.
 
@@ -132,9 +132,18 @@ std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> getStaticSamplers() {
       D3D12_TEXTURE_ADDRESS_MODE_CLAMP, // addressW
       0.0f,                             // mipLODBias
       8);                               // maxAnisotropy
+  const CD3DX12_STATIC_SAMPLER_DESC shadowPCFClamp(
+      6,                                         // shaderRegister
+      D3D12_FILTER_COMPARISON_MIN_MAG_LINEAR_MIP_POINT, // filter
+      D3D12_TEXTURE_ADDRESS_MODE_BORDER,         // addressU
+      D3D12_TEXTURE_ADDRESS_MODE_BORDER,         // addressV
+      D3D12_TEXTURE_ADDRESS_MODE_BORDER,         // addressW
+      0.0f,                                      // mipLODBias
+      16, D3D12_COMPARISON_FUNC_GREATER,
+      D3D12_STATIC_BORDER_COLOR_OPAQUE_BLACK); // maxAnisotropy
 
-  return {pointWrap,   pointClamp,      linearWrap,
-          linearClamp, anisotropicWrap, anisotropicClamp};
+  return {pointWrap,       pointClamp,       linearWrap,    linearClamp,
+          anisotropicWrap, anisotropicClamp, shadowPCFClamp};
 }
 
 void processSRV(nlohmann::json &jobj, CD3DX12_ROOT_PARAMETER &param) {
