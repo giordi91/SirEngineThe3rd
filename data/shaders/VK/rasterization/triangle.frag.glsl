7b7298cbb2cf6c3ecabfbee137d5215a5fe18eb5
Marco Giordano <marco.giordano.work@gmail.com>
2019-11-27 11:49:05 +0000

    fixed glsllang entry point, not sure why the behaviour is so weird

--- data/shaders/VK/rasterization/triangle.frag.glsl
+++ data/shaders/VK/rasterization/triangle.frag.glsl
@@ -7,7 +7,7 @@ layout(location=0) out vec4 outputColor;
 layout(location=0) in vec4 color;
 layout (location = 1) in vec2 inUV;
 
-void main()
+void PS()
 {
 	outputColor = texture(samplerColorMap, inUV);
 }
