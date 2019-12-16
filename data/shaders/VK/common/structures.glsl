be08bcd4565bc67b74e0dda7179caaa187643c0f
Marco Giordano <marco.giordano.work@gmail.com>
2019-12-16 00:00:00 +0000

    starting to work on uniform buffer to get camera movement

---
+++ data/shaders/VK/common/structures.glsl
@@ -0,0 +1,77 @@
+#ifndef STRUCTURES_HLSL
+#define STRUCTURES_HLSL
+
+struct CameraBuffer {
+  mat4 MVP;
+  mat4 ViewMatrix;
+  mat4 VPinverse;
+  vec4 perspectiveValues;
+  vec4 position;
+  float vFov;
+  float screenWidth;
+  float screenHeight;
+  float padding;
+};
+
+struct DirectionalLightData {
+  mat4 projectionMatrix;
+  mat4 lightVP;
+  mat4 worldToLocal;
+  mat4 localToWorld;
+  vec4 lightPosition;
+  vec4 lightDir;
+  vec4 lightColor;
+};
+
+// material buffer
+struct PhongMaterial {
+  vec4 kd;
+  vec4 ka;
+  vec4 ks;
+  // TODO pad shiness into ka.w
+  float shiness;
+  float reflective;
+  float roughnessMult;
+  float metallicMult;
+};
+
+struct DebugLayerConfig {
+  int stencilValue;
+};
+
+struct GammaToneMappingConfig {
+  float exposure;
+  float gamma;
+  float gammaInverse;
+  float padding;
+};
+
+struct TextureConfig {
+  int width;
+  int height;
+};
+struct ReducedDepth {
+  float minDepth;
+  float maxDepth;
+};
+
+struct SSSSSConfig {
+  float sssLevel;
+  float correction;
+  float maxdd;
+  float width;
+  vec2 direction;
+};
+
+struct DebugPointsFixedColor {
+  vec4 color;
+  float pointSize;
+  vec3 padding;
+};
+
+struct BoundingBox {
+  vec3 min;
+  vec3 max;
+  vec2 padding;
+};
+#endif
