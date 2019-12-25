954c89387fc3690d45e58d0fe6d2b61d568d15c7
Marco Giordano <marco.giordano.work@gmail.com>
2019-12-25 00:00:00 +0000

    fixed all psos can do a full resource rebuild now

--- data/shaders/DX12/rasterization/basicMeshVSTex.hlsl
+++ data/shaders/DX12/rasterization/basicMeshVSTex.hlsl
@@ -2,19 +2,24 @@
 #include "../common/vertexDefinitions.hlsl"
 
 ConstantBuffer<CameraBuffer> g_cameraBuffer : register(b0);
+StructuredBuffer<float4> g_positions : register(t8);
+StructuredBuffer<float4> g_normals : register(t9);
+StructuredBuffer<float2> g_uvs : register(t10);
+StructuredBuffer<float4> g_tangents : register(t11);
 
 
 FullMeshVertexOut VS( uint vid : SV_VertexID)
 {
 	FullMeshVertexOut vout;
 	
+    float4 p = g_positions[vid];
 	// Transform to homogeneous clip space.
-	vout.PosH = mul(float4(vin.PosL,1.0f), g_cameraBuffer.MVP);
+	vout.PosH = mul(p, g_cameraBuffer.MVP);
 	
 	// Just pass vertex color into the pixel shader.
-    vout.Normal= vin.Normal;
-	vout.uv = vin.uvs.xy;
-	vout.tangent = vin.tangents.xyz;
-	vout.worldPos = float4(vin.PosL,1.0f);
+    vout.Normal= g_normals[vid].xyz;
+	vout.uv = g_uvs[vid];
+	vout.tangent = g_tangents[vid].xyz;
+    vout.worldPos = p;
     return vout;
 }
