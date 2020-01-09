77c4062e210390c2e8f3ff3489856d6ebbd21d7b
Marco Giordano <marco.giordano.work@gmail.com>
2020-01-09 08:59:18 +0000

    trying to fix github actions

---
+++ ciSetup.ps1
@@ -0,0 +1,3 @@
+et-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+choco install --no-progress --yes vulkan-sdk
+
