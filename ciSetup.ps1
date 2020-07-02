398a881f425a04419d078718fecf7a2580d68881
Marco Giordano <marco.giordano.work@gmail.com>
2020-07-02 19:55:40 +0100

    fixing ci

--- ciSetup.ps1
+++ ciSetup.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
-choco install --no-progress --yes vulkan-sdk
+choco install vulkan-sdk  --no-progress --yes  --version=1.1.126.0
 [System.Environment]::SetEnvironmentVariable('VULKAN_SDK','C:\VulkanSDK\1.1.126.0',[System.EnvironmentVariableTarget]::User)
