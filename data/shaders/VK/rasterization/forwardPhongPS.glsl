91b6af92bbe75597b4d24ec989407e847a59164b
Marco Giordano <marco.giordano.work@gmail.com>
2020-07-18 00:00:00 +0000

    clean up of  shader, need to add more commenting for diffuse pbro

--- data/shaders/VK/rasterization/forwardPhongPS.glsl
+++ data/shaders/VK/rasterization/forwardPhongPS.glsl
@@ -2,6 +2,8 @@
 
 #extension GL_GOOGLE_include_directive: require
 #include "../common/structures.glsl"
+#include "../common/pbr.glsl"
+#include "../common/normals.glsl"
 
 layout (set=0,binding=0) uniform InputData 
 {
@@ -31,50 +33,6 @@ layout (location = 1) in vec3 tans;
 layout (location = 2) in vec2 inUV;
 layout (location = 3) in vec3 worldPos;
 
-const float PI = 3.14159265359;
-
-
-//F0 is the reflectance at zero angle of incidence, meaning
-//how much the surface reflects looking directly at it
-vec3 fresnelSchlick(float cosTheta, vec3 F0)
-{
-    return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}  
-
-float DistributionGGX(vec3 N, vec3 H, float roughness)
-{
-    float a      = roughness*roughness;
-    float a2     = a*a;
-    float NdotH  = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
-	
-    float num   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
-	
-    return num / denom;
-}
-
-float GeometrySchlickGGX(float NdotV, float roughness)
-{
-    float r = (roughness + 1.0);
-    float k = (r*r) / 8.0;
-
-    float num   = NdotV;
-    float denom = NdotV * (1.0 - k) + k;
-	
-    return num / denom;
-}
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
-{
-    float NdotV = max(dot(N, V), 0.0);
-    float NdotL = max(dot(N, L), 0.0);
-    float ggx2  = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1  = GeometrySchlickGGX(NdotL, roughness);
-	
-    return ggx1 * ggx2;
-}
-
 
 
 void PS()
@@ -87,7 +45,11 @@ void PS()
    vec3 V = normalize(cameraBuffer.position.xyz - worldPos);
    //half way vector
    vec3 H = normalize(L + V );
-   vec3 normal = normalize(inNormal);
+
+   //extract the normal taking into account the normal map
+   vec3 geometricNormal = normalize(inNormal);
+   vec3 tangent = normalize(tans);
+   vec3 normal = sampleNormalMap(colorSampler[2], tangentTex, geometricNormal,tangent , uv);
 
    //we don't compute light attenuation for a directional light
    //float dist = length(lightData.lightPosition.xyz - worldPos);
