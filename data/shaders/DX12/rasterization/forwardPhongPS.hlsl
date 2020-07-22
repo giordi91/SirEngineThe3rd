2983a6b378af678834078d980c23f755650cb169
Marco Giordano <marco.giordano.work@gmail.com>
2020-07-22 11:00:14 +0100

    ported pbr to dx12 correctly

--- data/shaders/DX12/rasterization/forwardPhongPS.hlsl
+++ data/shaders/DX12/rasterization/forwardPhongPS.hlsl
@@ -44,11 +44,53 @@ float4 PS(FullMeshVertexOut pin) : SV_Target {
   float3 toEyeDir = normalize(g_cameraBuffer.position.xyz - worldPos);
   float3 halfWay = normalize(toEyeDir + ldir);
   float3 reflected = reflect(-toEyeDir, pin.Normal);
+ // fresnel slick, ratio between specular and diffuse, it is tintend on
+  // metal, so we lerp toward albedo based on metallic, so only specular will
+  // be tinted by the albedo.
+  float3 F0 = 0.04f;
+  F0 = lerp(F0, albedo, metallic);
+  float3 F = fresnelSchlick(max(dot(normal, toEyeDir), 0.0f), F0, roughness);
+  float NDF = DistributionGGX(normal, halfWay, roughness);
+  float G = GeometrySmith(normal, toEyeDir, ldir, roughness);
 
+  // compute cook torrance
+  float3 nominator = NDF * G * F;
+  float denominator =
+  	4.0f * max(dot(normal, toEyeDir), 0.0f) * max(dot(normal, ldir), 0.0f) + 0.001f;
+  float3 specular = nominator / denominator;
+  
+  // compute specular
+  float3 kS = F;
+  float3 kD = 1.0f - kS;
+  kD *= 1.0f - metallic;
+  
+  // reflectance value
+  float3 Lo = 0.0f;
+  // single light for now
+  float3 radiance = g_dirLightData.lightColor.xyz;
+  float NdotL = max(dot(normal, ldir), 0.0f);
+  	Lo += (kD * (albedo / PI) + specular) * radiance * NdotL;
+  
+  //using irradiance map
+  float3 irradiance = skyboxIrradianceTexture.Sample(gsamLinearClamp,normal).xyz;
+  float3 diffuse      = irradiance* albedo;
+  
+  float MAX_REFLECTION_LOD = 8.0f;
+  float3 prefilteredColor = skyboxRadianceTexture.SampleLevel(gsamLinearClamp,reflected, roughness* MAX_REFLECTION_LOD).rgb;
+  float2 envBRDF = brdfTexture.Sample(gsamLinearClamp, float2(max(dot(normalize(normal), normalize(toEyeDir)), 0.0), roughness)).rg;
+  float3 specularDiff = prefilteredColor * (F* envBRDF.x + envBRDF.y);
+
+  float3 ambient = (kD * diffuse) + specularDiff;
+  float3 color = ambient + Lo;
+
+  return float4(color, 1.0f);
+
+  /*
   float4 color =
       albedoTex.Sample(gsamLinearClamp, float2(pin.uv.x, 1.0f - pin.uv.y));
   // return color;
   float3 l = -g_dirLightData.lightDir.xyz;
   float d = dot(l, pin.Normal);
   return float4(normal, 1.0f);
+  */
 }
