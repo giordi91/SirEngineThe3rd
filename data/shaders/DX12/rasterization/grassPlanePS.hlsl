6a558f8f3b9c8f19c1a29773b82ccfc10194ab69
Marco Giordano <marco.giordano.work@gmail.com>
2020-09-24 00:00:00 +0000

    correctly rebuilding metadata for vk

--- data/shaders/DX12/rasterization/grassPlanePS.hlsl
+++ data/shaders/DX12/rasterization/grassPlanePS.hlsl
@@ -1,5 +1,7 @@
 #include "../common/vertexDefinitions.hlsl"
+#include "../common/structures.hlsl"
 
+ConstantBuffer<GrassConfig> grassConfig: register(b3,space3);
 Texture2D sourceTexture: register(t4,space3);
 
 SamplerState gsamPointWrap        : register(s0,space1);
@@ -8,6 +10,12 @@ SamplerState gsamLinearWrap       : register(s2,space1);
 SamplerState gsamLinearClamp      : register(s3,space1);
 SamplerState gsamAnisotropicWrap  : register(s4,space1);
 SamplerState gsamAnisotropicClamp : register(s5,space1);
+
+ConstantBuffer<FrameData> g_frameData : register(b0,space0);
+ConstantBuffer<DirectionalLightData> g_dirLightData : register(b0, space2);
+TextureCube skyboxIrradianceTexture : register(t1, space2);
+TextureCube skyboxRadianceTexture : register(t2, space2);
+Texture2D brdfTexture : register(t3, space2);
 
 float4 PS(FullScreenVertexOut pin) : SV_Target
 {
