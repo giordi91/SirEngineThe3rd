c5c6931fab9604989594ac7b47ccd04fdadf5d14
Marco Giordano <marco.giordano.work@gmail.com>
2020-10-23 00:00:00 +0000

    cleaned up debug layer using new imgui stuff

--- Editor/src/editor.cpp
+++ Editor/src/editor.cpp
@@ -6,18 +6,18 @@
 #include "SirEngine/log.h"
 
 #include "SirEngine/entryPoint.h"
+#include "SirEngine/Layers/imguiDebugLayer.h"
 
 class Editor final : public SirEngine::Application {
 public:
   Editor(): Application()
   {
-  //imGuiLayer = new ImguiLayer();
+  //imGuiLayer = new SirEngine::ImguiLayer();
   imGuiLayer = new SirEngine::EditorLayer();
   graphicsLayer = new SirEngine::GraphicsLayer();
   m_layerStack.pushLayer(graphicsLayer);
   m_layerStack.pushLayer(imGuiLayer);
-	  
-  };
+  }
   ~Editor() = default;
 };
 
